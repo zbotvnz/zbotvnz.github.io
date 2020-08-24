9f21c1999acb4cdcaa1fbf4e40daa55a19de336e
Duong Le <zbotvnz@gmail.com>
2020-08-24 15:43:27 +0700

    Update music

--- songs.js
+++ songs.js
@@ -3,5 +3,11 @@ var songs = [{
 		name: "Lời Chưa Nói",
 		url: "https://drive.google.com/uc?export=download&id=1_oUwVxLRxyyf9GlD3yTtGC5Vu9bgyEfP",
 		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},
+	{
+		artist: "Soobin Hoàng Sơn",
+		name: "Say Goodbye",
+		url: "https://drive.google.com/uc?export=download&id=1F-smbuj__v7_1Q-Mag05-dQJWD77q6w4",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
 	}
 ];
