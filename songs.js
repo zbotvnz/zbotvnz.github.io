6ce5b82418756b8e93ec865e80582170eacd8965
Duong Le <zbotvnz@gmail.com>
2020-08-24 16:20:46 +0700

    update ms

--- songs.js
+++ songs.js
@@ -9,5 +9,11 @@ var songs = [{
 		name: "Say Goodbye",
 		url: "https://drive.google.com/uc?export=download&id=1F-smbuj__v7_1Q-Mag05-dQJWD77q6w4",
 		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
+	},
+	{
+		artist: "Trịnh Đình Quang",
+		name: "Nếu Không Thể Đến Với Nhau",
+		url: "https://drive.google.com/uc?export=download&id=1QiKiH_ibtxgUmULtKzeBLdJ-K03BpOUR",
+		picture: "https://raw.githubusercontent.com/himalayasingh/music-player-1/master/img/_1.jpg"
 	}
 ];
